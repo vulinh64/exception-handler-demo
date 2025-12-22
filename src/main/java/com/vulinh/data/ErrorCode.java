ba56462224a407873b5977490a7f90f11ffe8504
Linh Nguyen <nguyenvulinh64@outlook.com>
2025-12-22 16:02:01 +0700

    Add 404 HTTP Status handling

--- src/main/java/com/vulinh/data/ErrorCode.java
+++ src/main/java/com/vulinh/data/ErrorCode.java
@@ -9,7 +9,8 @@ public enum ErrorCode {
   M000("Success"),
   M999("Internal Server Error"),
   M001("Data existed"),
-  M400("Bad data");
+  M400("Bad data"),
+  M404("Not found");
 
   private final String message;
 }
--- src/main/java/com/vulinh/exception/GlobalExceptionHandler.java
+++ src/main/java/com/vulinh/exception/GlobalExceptionHandler.java
@@ -30,11 +30,11 @@ public class GlobalExceptionHandler {
   }
 
   @ExceptionHandler(NotFound404Exception.class)
-  public ResponseEntity<Void> handleNotFound401Exception(NotFound404Exception ex) {
+  public ResponseEntity<Object> handleNotFound401Exception(NotFound404Exception ex) {
     showExceptionMessage(ex);
 
     // Another way to return HttpStatus without @ResponseStatus
-    return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(fromErrorCode(ErrorCode.M404));
   }
 
   @ExceptionHandler(IllegalArgumentException.class)
